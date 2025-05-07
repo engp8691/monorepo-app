0bad595e26af84e664db83109228bb4467a50d98
Yonglin Lee <ylpro@Mac.hsd1.ma.comcast.net>
2025-05-07 19:10:38 -0400

    feat: todo list with re-rendering

--- apps/frontend/src/pages/About.tsx
+++ apps/frontend/src/pages/About.tsx
@@ -3,7 +3,7 @@ import { Todos } from '../components/ToDo'
 const About: React.FC = () => {
     return (
         <div>
-            <h1>The todo list has not re-rendering when add or remove a todo</h1>
+            <h1>The todo list has not re-rendering when add or remove a todo. Please check the console log</h1>
             <Todos />
         </div>
     )
