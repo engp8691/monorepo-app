370be83bc5db4131094053e473975ef031ab73f3
yonglin li <yonglin.li@gmail.com>
2025-05-20 17:15:57 -0400

    feat: add playwright e2e tests (#14)
    * feat: add playwright e2e tests
    
    * fix: make slowMo less time
    
    * fix: change the port back
    
    * feat: more ways to local radio buttons and check box
    
    * fix: launch it headless
    
    ---------
    
    Co-authored-by: Yonglin Lee <ylpro@Mac.hsd1.ma.comcast.net>

---
+++ apps/frontend-e2e/src/form.spec.ts
@@ -0,0 +1,63 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/')
+
+  await page.click('text=Home')
+  await page.click('text=Form 1')
+})
+
+test('should show errors after clicking on submit button with empty form', async ({
+  page,
+}) => {
+  await page.click('button:has-text("Submit")')
+
+  await expect(page).toHaveURL(/form1/)
+  await expect(page.locator('text=Name is required')).toBeVisible()
+  await expect(page.locator('text=Email is required')).toBeVisible()
+  await expect(page.locator('text=Age is required')).toBeVisible()
+  await expect(page.locator('text=Gender is required')).toBeVisible()
+  await expect(page.locator('text=Country is required')).toBeVisible()
+  await expect(
+    page.locator('text=You must accept the terms and conditions'),
+  ).toBeVisible()
+})
+
+test('should submit the form without error with all fields filled', async ({
+  page,
+}) => {
+  await expect(page).toHaveURL(/form1/)
+
+  await page.click('button:has-text("Submit")')
+  await page.fill('input[name="name"]', 'John Doe')
+  await page.fill('input[name="email"]', 'john')
+  await expect(page.locator('text="Invalid email address"')).toBeVisible()
+  await page.fill('input[name="email"]', 'john@example.com')
+  await page.fill('input[name="age"]', '13')
+  await expect(
+    page.locator('text="You must be at least 18 years old"'),
+  ).toBeVisible()
+  await page.fill('input[name="age"]', '32')
+  await page.locator('[data-testid="male"]').click()
+  // three ways to locate the rodio button
+  await page.getByText(/^Male$/, { exact: true }).click()
+  await page.getByText('Female').click()
+  await page.selectOption('select[name="country"]', 'USA')
+  await page.selectOption('select[name="state"]', 'New York')
+  // two ways to locate the check box
+  await page
+    .getByText(/^I accept the terms and conditions$/, { exact: true })
+    .check()
+  await page.locator('[data-testid="acceptedTerms"]').check()
+
+  await page.click('button:has-text("Submit")')
+
+  await expect(page.locator('text=Name is required')).toHaveCount(0)
+  await expect(page.locator('text=Email is required')).toHaveCount(0)
+  await expect(page.locator('text=Age is required')).toHaveCount(0)
+  await expect(page.locator('text=Gender is required')).toHaveCount(0)
+  await expect(page.locator('text=Country is required')).toHaveCount(0)
+  await expect(
+    page.locator('text=You must accept the terms and conditions'),
+  ).toHaveCount(0)
+})
